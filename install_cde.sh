ef4b290c7603a81bba0b2fa6f60e6f474bc51475
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2022-01-09 12:52:11 +0100

    Put all codeblocks in functions

--- install_cde.sh
+++ install_cde.sh
@@ -24,95 +24,107 @@
 # https://sourceforge.net/p/cdesktopenv/wiki/CDE%20on%20the%20Raspberry%20Pi/ 
 # section 3 for more info.
 
-#------------------------------------------------------------------------------ 
-# Generate locale support
-#------------------------------------------------------------------------------ 
 
-sudo locale-gen de_DE
-sudo locale-gen es_ES
-sudo locale-gen fr_FR
-sudo locale-gen it_IT
-sudo locale-gen de_DE.UTF-8
-sudo locale-gen es_ES.UTF-8
-sudo locale-gen fr_FR.UTF-8
-sudo locale-gen it_IT.UTF-8
+function preprocess {
+    #------------------------------------------------------------------------------ 
+    # Generate locale support
+    #------------------------------------------------------------------------------ 
+    sudo locale-gen de_DE
+    sudo locale-gen es_ES
+    sudo locale-gen fr_FR
+    sudo locale-gen it_IT
+    sudo locale-gen de_DE.UTF-8
+    sudo locale-gen es_ES.UTF-8
+    sudo locale-gen fr_FR.UTF-8
+    sudo locale-gen it_IT.UTF-8
 
-#------------------------------------------------------------------------------ 
-# Install needed packages
-#------------------------------------------------------------------------------ 
-
-apt-get -y install \
-    autoconf \
-    automake \
-    bison \
-    build-essential \
-    flex
-    g++ \
-    git \
-    ksh \
-    libtool \
-    lib{xt,xmu,xft,xinerama,xpm,pam,motif,ssl,xaw7,x11,xss,tirpc,jpeg,freetype6,utempter}-dev \
-    m4 \
-    ncompress \
-    patch \
-    rpcbind \
-    tcl-dev \
-    x11proto-fonts-dev \
-    xbitmaps \
-    xfonts-{100,75}dpi{,-transcoded} \
-    xorg \
-
-#------------------------------------------------------------------------------ 
-# Compile and install CDE
-#------------------------------------------------------------------------------ 
-
-pushd /tmp
-
-git clone https://git.code.sf.net/p/cdesktopenv/code cdesktopenv-code
-cd cdesktopenv-code/cde
-
-./autogen.sh
-./configure
-make -j4
-sudo make install
-
-popd
+    #------------------------------------------------------------------------------ 
+    # Install needed packages
+    #------------------------------------------------------------------------------ 
+    apt-get -y install \
+        autoconf \
+        automake \
+        bison \
+        build-essential \
+        flex
+        g++ \
+        git \
+        ksh \
+        libtool \
+        lib{xt,xmu,xft,xinerama,xpm,pam,motif,ssl,xaw7,x11,xss,tirpc,jpeg,freetype6,utempter}-dev \
+        m4 \
+        ncompress \
+        patch \
+        rpcbind \
+        tcl-dev \
+        x11proto-fonts-dev \
+        xbitmaps \
+        xfonts-{100,75}dpi{,-transcoded} \
+        xorg 
+}
 
 
-#------------------------------------------------------------------------------ 
-# Set default display manager on Raspberry Pi
-#------------------------------------------------------------------------------ 
+function install_cde {
+    #------------------------------------------------------------------------------ 
+    # Compile and install CDE
+    #------------------------------------------------------------------------------ 
+    pushd /tmp
 
-sudo mv /etc/X11/default-display-manager /etc/X11/default-display-manager.bu
-sudo echo "/usr/dt/bin/dtlogin" > /etc/X11/default-display-manager
+    git clone https://git.code.sf.net/p/cdesktopenv/code cdesktopenv-code
+    cd cdesktopenv-code/cde
 
-sudo cp ./resources/systemd/dtlogin.service /lib/systemd/system/dtlogin.service
-sudo ln -svf /lib/systemd/system/dtlogin.service /etc/systemd/system/display-manager.service
-sudo ln -svf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+    ./autogen.sh
+    ./configure
+    make -j4
+    sudo make install
 
-#------------------------------------------------------------------------------ 
-# Improve fonts of the interface
-#------------------------------------------------------------------------------ 
+    popd
+}
 
-sudo apt install gsfonts gsfonts-x11 ttf-mscorefonts-installer fonts-powerline
-sudo mv /usr/dt/config/xfonts/C/fonts.alias /usr/dt/config/xfonts/C/fonts.alias.bu
-sudo cp ./resources/fonts/fonts.alias /usr/dt/config/xfonts/C/fonts.alias
+function set_display_manager {
+    #------------------------------------------------------------------------------ 
+    # Set default display manager 
+    #------------------------------------------------------------------------------ 
+    sudo mv /etc/X11/default-display-manager /etc/X11/default-display-manager.bu
+    sudo echo "/usr/dt/bin/dtlogin" > /etc/X11/default-display-manager
+    sudo cp ./resources/systemd/dtlogin.service /lib/systemd/system/dtlogin.service
+    sudo ln -svf /lib/systemd/system/dtlogin.service /etc/systemd/system/display-manager.service
+    sudo ln -svf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+}
+
+function improve_fonts {
+    #------------------------------------------------------------------------------ 
+    # Improve fonts of the interface
+    #------------------------------------------------------------------------------ 
+    sudo apt install gsfonts gsfonts-x11 ttf-mscorefonts-installer fonts-powerline
+    sudo mv /usr/dt/config/xfonts/C/fonts.alias /usr/dt/config/xfonts/C/fonts.alias.bu
+    sudo cp ./resources/fonts/fonts.alias /usr/dt/config/xfonts/C/fonts.alias
+}
 
 #------------------------------------------------------------------------------ 
 # Add Motif theme for GTK
 #------------------------------------------------------------------------------ 
+function set_motif_lookandfeel {
+    mkdir -p ${HOME}/.themes
+    unzip ./resources/gtk/cdetheme1.3.tar.gz ${HOME}/.themes/
+}
 
-mkdir -p ${HOME}/.themes
-unzip ./resources/gtk/cdetheme1.3.tar.gz ${HOME}/.themes/
+function add_additional_software {
+    #------------------------------------------------------------------------------ 
+    # Install some additional X11 software from the mid 90s era.
+    #------------------------------------------------------------------------------ 
+    sudo apt install -y \
+        xfig \
+        xpdf
 
-#------------------------------------------------------------------------------ 
-# Install some additional X11 software from the mid 90s era.
-#------------------------------------------------------------------------------ 
+    sudo apt install snapd
+    sudo snap install core
+    sudo snap install xv --edge
+}
 
-sudo apt install -y \
-    xfig \
-    xpdf
-
-sudo apt install snapd
-sudo snap install core
-sudo snap install xv --edge
+preprocess
+install_cde
+set_display_manager
+improve_fonts
+set_motif_lookandfeel
+add_additional_software
